69173717d075bf05449559f8372b5bbe72fcd0ee
Anthony Ochoa <ochoaap@yahoo.com>
2020-10-15 22:17:48 -0700

    Continous updates

--- js/app.js
+++ js/app.js
@@ -1,27 +1,36 @@
 'use strict';
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am','12pm', '1pm', '2pm','3pm', '4pm', '5pm', '6pm', '7pm'];
 
 var seattle = {
   name: 'Seattle',
   min: 23,
   max: 65,
   avgCookies: 6.3,
-  randomCust: 0,
-  avgPerCust: 0,
-  cookiesPerHour: 0,
+  customerPerHour: [],
+  //randomCust: 0,
+  //avgPerCust: 0,
+  cookiesPerHour: [],
   totalPerDay: 0,
-  hours: ['6am', '7am', '8am', '9am', '10am', '11am','12pm', '1pm', '2pm','3pm', '4pm', '5pm', '6pm', '7pm'],
-
   getCust: function () {
-    seattle.randomCust = customerPerDay(23,65);
-  },
-  getcookies: function() {
-  cookiesPerHour = 
+    for(var i = 0; i < hours.length; i++) {
+      this.customerPerHour.push(getcustvalue(this.min,this.max));
+    }
+  getCookies: function () {
+    for(var i = 0; i < hours.length; i++) {
+      this.cookiesPerHour.push(getCookvalue(this.customerPerHour,this.avgCookies));
+    }
   }
-
+  },
 };
+seattle.getCust();
 
-function customerPerDay (min, max) {
-  return Math.floor(Math.random() * (max - min + 1)
- + min);
+// received this function from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getcustvalue (min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+  //The maximum is inclusive and the minimum is inclusive
 }
-function cookiesPerHour (customerPerDay, avgCookies)
+function getCookies ()
+
+console.log(seattle)
