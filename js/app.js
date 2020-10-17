a219cacc886ea4ec6a63229cb0b7a311fbbe048f
Anthony Ochoa <ochoaap@yahoo.com>
2020-10-17 10:34:33 -0700

    creating branch

--- js/app.js
+++ js/app.js
@@ -225,7 +225,7 @@ function getcustvalue(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
   //The maximum is inclusive and the minimum is inclusive
 }
-console.log(tokyo);
+//console.log(tokyo);
 
 
 var parentElement = document.getElementById('lima');
